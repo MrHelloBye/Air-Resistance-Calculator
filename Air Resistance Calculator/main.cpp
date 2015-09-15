cc309bf18d52878b4cd321645df1c6b4f2ae8285
Liam Clink <MrHelloBye@Liams-MacBook-Pro.local>
2015-09-14 22:46:35 -0400

    Changing over to actual code

--- Air Resistance Calculator/main.cpp
+++ Air Resistance Calculator/main.cpp
@@ -1,17 +1,57 @@
-//
-//  main.cpp
-//  Air Resistance Calculator
-//
-//  Created by Liam Clink on 9/14/15.
-//  Copyright (c) 2015 Liam Clink. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+const double pi = 3.14159265358;
+
+double Calcv0y(double v0, double theta) {
+    return v0*sin(theta);
+}
+double Calcv0x(double v0, double theta) {
+    return v0*cos(theta);
+}
+
+double Calcy(double t, double gamma, double v0y) {
+    return -9.81*t/gamma+(v0y+9.81/gamma)/gamma*(1-exp(-gamma*t)); // Defines vertical position
+}
+double Calcvy(double t, double gamma, double v0y) {
+    return (v0y+9.81/gamma)*exp(-gamma*t)-9.81/gamma; // Defines Vertical velocity
+}
+
+double Calcx(double t, double gamma, double v0x) {
+    return v0x/gamma*(1-exp(-gamma*t)); // Defines horizontal position.
+}
+double Calcvx(double t, double gamma, double v0x) {
+    return v0x*exp(-gamma*t);
+}
+
+int main() {
+    double delt, tguess = 0, tolerance = 1.0E-8;
+    double v0, theta, thetaInRads, gamma, height;
+    bool repeatCheck;
+    double x, y, v0x, v0y, vx, vy;
+    
+    do {
+        cout << "Enter v0 in m/s:\n";
+        scanf("%lf", & v0);
+        cout << "Enter theta in degrees:\n";
+        scanf("%lf", & theta);
+        thetaInRads = theta/180*pi;
+        cout << "Enter gamma in s^-1:\n";
+        scanf("%lf", & gamma);
+        cout << "Enter height in meters:\n";
+        scanf("%lf", & height);
+        
+        for (int repeatCounter; (repeatCounter <= 20) || (delt <= tolerance); repeatCounter++) {
+            // Perform Newton's Method.
+            y = Calcy(tguess, gamma, v0y);
+            if (y) {
+                
+            }
+        }
+        
+    } while (repeatCheck == true);
     return 0;
 }
